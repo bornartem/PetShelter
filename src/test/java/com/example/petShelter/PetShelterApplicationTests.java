10d56701c2abf3caa1ad81db3c6994f4d579bf5e
bornartem <born.artem@gmail.com>
2024-03-22 20:26:54 +0300

    1 commit

---
+++ src/test/java/com/example/petShelter/PetShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petShelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetShelterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
