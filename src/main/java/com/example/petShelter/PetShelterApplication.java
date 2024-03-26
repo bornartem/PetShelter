41b15634313f1a0e36db2a2c0fbc39614357de22
khilola.kushbakova <xilola_kushbakova@mai.ru>
2024-03-26 12:41:31 +0100

    added entities

--- src/main/java/com/example/petShelter/PetShelterApplication.java
+++ src/main/java/com/example/petShelter/PetShelterApplication.java
@@ -8,10 +8,7 @@ public class PetShelterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetShelterApplication.class, args);
-		//test
 
-//		danil edit
-		// la la  la la
 	}
 
 }
