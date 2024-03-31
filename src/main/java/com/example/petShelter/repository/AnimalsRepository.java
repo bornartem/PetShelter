6dcc9173da8ef66e67ed558c788f421909239f16
bkvdm <vadim@991199.ru>
2024-04-01 01:06:37 +0300

    Добавил классы-интерфейсы JpaRepository для классов моделей.

--- src/main/java/com/example/petShelter/repository/AnimalsRepository.java
+++ src/main/java/com/example/petShelter/repository/AnimalsRepository.java
@@ -1,4 +1,11 @@
 package com.example.petShelter.repository;
 
-public class AnimalsRepository {
+import com.example.petShelter.model.Animals;
+import com.example.petShelter.model.DailyReports;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface AnimalsRepository extends JpaRepository<Animals, Long> {
 }
