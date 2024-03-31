891561942a8b84f740fcf79e23170c3ea5468cb1
hirof <hirofool502@gmail.com>
2024-03-31 11:33:00 +0300

    start create call volunteers

---
+++ src/main/java/com/example/petShelter/conversationServices/SendMessageInConversation.java
@@ -0,0 +1,12 @@
+package com.example.petShelter.conversationServices;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.request.SendMessage;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SendMessageInConversation {
+
+
+
+}
---
+++ src/main/java/com/example/petShelter/conversationServices/StartService.java
@@ -0,0 +1,104 @@
+package com.example.petShelter.conversationServices;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class StartService {
+
+    @Autowired
+    VolunteerService volunteerService;
+
+    @Autowired
+    ClientService clientService;
+
+    private TelegramBot telegramBot;
+    SendMessageInConversation sendMessageInConversation;
+
+    private String HIMESSAGE = "поприветствуйте клиента, пердставтесь и ответьте на вопрос.";
+    private String STOPMESSAGE = "Диалог завершен";
+    private String WEFINDVOLUNTEER = "Ищем свободного волонтера для вас";
+
+    /**
+     * метод в начале когда пришло сообщение
+     * о том что пользователь вызывает волонтера
+     *
+     * @param update пользователь который просит о помощи
+     */
+    public void firstGivingUsers(Update update) {
+        Long ClientId = update.message().chat().id();
+        String clientText = update.message().text();
+
+        Long volId;
+        volId = findRelaxVolunteers();
+        if (volId == null) {
+            //добавляем в ожидающих
+        } else {
+
+            telegramBot.execute(new SendMessage(ClientId, WEFINDVOLUNTEER));
+
+            //делает волонтера неактивным
+            volunteerService.disactiveVolunteer(volId);
+
+            //добавил их в таблицу общающихся
+            usersToConversationTable(ClientId, volId);
+
+
+
+            //отправить волонтеру что он должен первое написать
+            telegramBot.execute(new SendMessage(
+                    volId,
+                    HIMESSAGE
+            ));
+
+        }
+    }
+
+    /**
+     * добавляем в таблицу общающихся пользователей
+     * клиента и волонтера
+     *
+     * @param clientId id клиента not null
+     * @param volId id волонтера not null
+     */
+    private void usersToConversationTable(Long clientId, Long volId) {
+//        id генерируется само
+        conversationPeopleService.addPeople(clientId, volId);
+        conversationPeopleService.addPeople(volId, clientId);
+    }
+
+    /**
+     * метод который ищет свободного волонтера
+     * если такого не находится возвращается null
+     * @return id свободного волнтера или null
+     */
+    private Long findRelaxVolunteers() {
+        // поиск по таблице(ее пока что нет)
+        return null;
+    }
+
+
+
+    /**
+     * если в листенере пришло сообщение от пользователя
+     * который есть в таблице общающихся пользователей
+     *
+     * @param update      пользователь написавший сообщение
+     * @param isVolunteer является пользователь волонтером
+     */
+    public void method(Update update, Boolean isVolunteer) {
+        Long updateId = update.message().chat().id();
+        String message = update.message().text();
+        if (isVolunteer && Objects.equals(message, "/stop")) {
+            telegramBot.execute(new SendMessage(updateId, STOPMESSAGE));
+            //найти в таблице по updateId с кем общается и ему тоже отправить
+            //удалить из общающихся таблицы обоих
+
+        }
+    }
+}
