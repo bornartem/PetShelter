774d4f63b1b3714f468f2993047a6ca5bdc01a4a
bornartem <138793957+bornartem@users.noreply.github.com>
2024-04-01 21:49:24 +0300

    Delete src/main/java/com/example/petShelter/command directory

--- src/main/java/com/example/petShelter/command/Command.java
+++
@@ -1,8 +0,0 @@
-package com.example.petShelter.command;
-
-import com.pengrad.telegrambot.model.CallbackQuery;
-import com.pengrad.telegrambot.model.Message;
-
-public interface Command {
-    void execute(Message message, CallbackQuery callbackQuery);
-}
