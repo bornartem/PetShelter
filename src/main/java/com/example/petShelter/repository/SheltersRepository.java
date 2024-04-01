e72b29507f297c323aa04e02a85c94d5daff77c6
bornartem <138793957+bornartem@users.noreply.github.com>
2024-04-01 21:51:42 +0300

    Delete src/main/java/com/example/petShelter/repository directory

--- src/main/java/com/example/petShelter/repository/SheltersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.petShelter.repository;
-
-import com.example.petShelter.model.Shelters;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SheltersRepository extends JpaRepository<Shelters, Long> {
-}
