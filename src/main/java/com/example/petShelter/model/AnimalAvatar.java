400400d626f75757ae83a51d3958d084d4ccc274
Мария <maria.sinyavskaya8@gmail.com>
2024-04-03 20:49:47 +0300

    Внесены правки и исправления, программа компилируется, телеграм бот отвечает на команду /start

---
+++ src/main/java/com/example/petShelter/model/AnimalAvatar.java
@@ -0,0 +1,23 @@
+package com.example.petShelter.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class AnimalAvatar {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String filePath;
+    private long fileSize;
+    private String mediaType;
+    @Lob
+    private byte[] data;
+    @OneToOne
+    private Animals animal;
+
+}
---
+++ src/main/java/com/example/petShelter/model/Animals.java
@@ -0,0 +1,38 @@
+package com.example.petShelter.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Entity(name = "animals")
+public class Animals {
+
+    @JsonIgnore
+    @ManyToOne
+//    @JoinColumn(name = "id_shelter")
+    private Shelters shelter;
+
+//    @OneToMany(mappedBy = "daily_report")
+    @OneToMany(mappedBy = "animal")
+    private List<DailyReports> dailyReport;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_animal")
+    private long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "type")
+    private String type;
+    @Column(name = "busy_free", nullable = false)
+    private Boolean busyFree;
+    @Column(name = "date_take")
+    private LocalDateTime dateTake;
+}
