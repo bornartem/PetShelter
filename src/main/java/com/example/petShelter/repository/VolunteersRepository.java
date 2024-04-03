e234e557f1c21537989425eeb2e854d7076e137b
MariaSinyavskaya <138773009+MariaSinyavskaya@users.noreply.github.com>
2024-04-03 20:45:43 +0300

    Delete src/main/java/com/example/petShelter/repository directory

--- src/main/java/com/example/petShelter/repository/VolunteersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.petShelter.repository;
-
-import com.example.petShelter.model.Volunteers;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VolunteersRepository extends JpaRepository<Volunteers, Long> {
-}
