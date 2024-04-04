0488a4fd4b2230b5851b6668a0c6467f637fe9c4
hirof <hirofool502@gmail.com>
2024-04-04 10:16:11 +0300

    delete not need things. only need it.

--- src/main/java/com/example/petShelter/repository/AnimalAvatarRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.petShelter.repository;
-
-import com.example.petShelter.model.AnimalAvatar;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnimalAvatarRepository  extends JpaRepository<AnimalAvatar, Long> {
-}
