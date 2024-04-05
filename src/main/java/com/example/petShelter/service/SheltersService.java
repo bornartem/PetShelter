eb3f9492cf3c74fb758616cdd626eb84852e4b70
Мария <maria.sinyavskaya8@gmail.com>
2024-04-05 16:11:41 +0300

    Наброски создания меню телеграм бота

--- src/main/java/com/example/petShelter/service/SheltersService.java
+++ src/main/java/com/example/petShelter/service/SheltersService.java
@@ -3,6 +3,8 @@ package com.example.petShelter.service;
 import com.example.petShelter.model.Shelters;
 import com.example.petShelter.repository.SheltersRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,7 +25,7 @@ public class SheltersService {
     @Autowired
     private final SheltersRepository sheltersRepository;
 
-
+    private Logger log = LoggerFactory.getLogger(SheltersService.class);
 
     public SheltersService(SheltersRepository sheltersRepository) {
         this.sheltersRepository = sheltersRepository;
