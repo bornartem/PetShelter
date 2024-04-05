4b3fb7dc6af2c7a58fc0dfddf0676b792970bffb
Мария <maria.sinyavskaya8@gmail.com>
2024-04-05 16:11:41 +0300

    Наброски создания меню телеграм бота

--- src/main/java/com/example/petShelter/command/CommandContainer.java
+++ src/main/java/com/example/petShelter/command/CommandContainer.java
@@ -16,9 +16,9 @@ public class CommandContainer {
 
     private TelegramBotClient telegramBotClient;
 
-    public CommandContainer(StartCommand commandStart) {
+    public CommandContainer(StartCommand commandStart, GetAddressOfDogShelterCommand getAddressOfDogShelterCommand) {
         commandMap.put(CommandName.START.getCommandName(), commandStart);
-
+        commandMap.put(CommandName.GET_ADDRESS_OF_DOG_SHELTER.getCommandName(), getAddressOfDogShelterCommand);
     }
 
     public void process(String commandName, Message message, CallbackQuery callbackQuery) {
