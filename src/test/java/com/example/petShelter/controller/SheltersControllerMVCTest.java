b2bc37e77788dc0b1b9a34ff05b9153c6b33c0c4
hirof <hirofool502@gmail.com>
2024-04-15 12:14:16 +0300

    commit

--- src/test/java/com/example/petShelter/controller/SheltersControllerMVCTest.java
+++ src/test/java/com/example/petShelter/controller/SheltersControllerMVCTest.java
@@ -1,7 +1,9 @@
 package com.example.petShelter.controller;
 
+import com.example.petShelter.model.Shelters;
 import com.example.petShelter.repository.*;
 import com.example.petShelter.service.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,11 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
@@ -72,5 +79,35 @@ public class SheltersControllerMVCTest {
 
 
     }
+
+    List<Shelters> shelters = new ArrayList<>(List.of());
+
+    @BeforeEach
+    public void initColl() {
+        shelters.add(
+                new Shelters(new ArrayList<>(), 10L, "one", "two", "null", "null", "null", "null", "null")
+        );
+        shelters.add(
+                new Shelters(null, 20L, "one", "two", null, null, null, null, null)
+        );
+        shelters.add(
+                new Shelters(null, 30L, "one", "two", null, null, null, null, null)
+        );
+    }
+
+    @Test
+    public void listAllShelters() {
+
+        String request = "/all-shelters";
+
+        when(sheltersRepository.findAll()).thenReturn(shelters);
+
+//        mockMvc.perform(
+//                get(request).accept(paramFinish));
+//
+//
+
+    }
+
 
 }
