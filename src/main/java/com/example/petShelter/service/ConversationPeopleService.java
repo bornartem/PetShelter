0eacc13d27e6a2ccdb88830fdba315f67254151e
hirof <hirofool502@gmail.com>
2024-04-21 20:41:13 +0300

    start create merge with old branch

---
+++ src/main/java/com/example/petShelter/service/ConversationPeopleService.java
@@ -0,0 +1,28 @@
+package com.example.petShelter.service;
+
+import com.example.petShelter.model.ConversationPeople;
+import com.example.petShelter.repository.ConversationPeopleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConversationPeopleService {
+    private final ConversationPeopleRepository conversationRepository;
+
+    @Autowired
+    public ConversationPeopleService(ConversationPeopleRepository conversationRepository) {
+        this.conversationRepository = conversationRepository;
+    }
+
+    public void savePeople(Long chatId, Long chatId2, boolean isVolunteer) {
+        conversationRepository.save(new ConversationPeople(Integer.MAX_VALUE, chatId, chatId2, isVolunteer));
+    }
+
+    public void deletePeople(Long chatId) {
+        conversationRepository.deleteByChatId(chatId);
+    }
+
+    public ConversationPeople findByChatId(Long chatId) {
+        return conversationRepository.findByChatId(chatId);
+    }
+}
